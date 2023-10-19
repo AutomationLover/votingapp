685689419a2df45e2fcf52993346a524b74bd29e
William Wang <hahawangxiao@gmail.com>
2023-10-19 11:07:53 +1100

    Create del.sh

---
+++ del.sh
@@ -0,0 +1,14 @@
+kubectl delete -f result-app-service.yml
+kubectl delete -f voting-app-service.yml
+
+kubectl delete -f redis-service.yml
+kubectl delete -f postgres-service.yml
+
+kubectl delete -f postgres-deployment.yml
+kubectl delete -f voting-app-deployment.yml
+kubectl delete -f worker-app-deployment.yml
+
+kubectl delete -f redis-deployment.yml
+
+kubectl delete -f result-app-deployment.yml
+kubectl delete -f namespace-deploy.yaml
